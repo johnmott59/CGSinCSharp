8cc3d4495412abdbff253047d12505163341cf01
johnmott59 <johnkmott@gmail.com>
2016-09-05 09:15:34 -0500

    Update CSGVertex.cs

--- CSGVertex.cs
+++ CSGVertex.cs
@@ -18,44 +18,22 @@ namespace CSGV1
 
         }
 
-#if false
-        CSG.Vertex = function(pos, normal) {
-            this.pos = new CSG.Vector(pos);
-            this.normal = new CSG.Vector(normal);
-        };
-#endif
         public CSGVertex(float[] pos,float[] normal)
         {
             this.pos = new CSGVector(pos);
             this.normal = new CSGVector(normal);
         }
-#if false
-         clone: function() {
-        return new CSG.Vertex(this.pos.clone(), this.normal.clone());
-        },
-#endif
+
         public CSGVertex clone()
         {
             return new CSGVertex(this.pos.clone(), this.normal.clone());
         }
 
-#if false
-  flip: function() {
-    this.normal = this.normal.negated();
-  },
-#endif
         public void flip()
         {
             this.normal = this.normal.negated();
         }
-#if false
-    interpolate: function(other, t) {
-        return new CSG.Vertex(
-            this.pos.lerp(other.pos, t),
-            this.normal.lerp(other.normal, t)
-    );
-  }
-#endif
+
         public CSGVertex interpolate(CSGVertex other,float t)
         {
             return new CSGVertex(this.pos.lerp(other.pos, t), this.normal.lerp(other.normal, t));
